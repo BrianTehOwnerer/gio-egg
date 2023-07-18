f7becec6b5c1fabbe18fa18811eb1d871ace2b4e
Brian <brian@debian>
2023-07-18 14:46:02 -0500

    ch4-button on bottom

--- main.go
+++ main.go
@@ -15,33 +15,55 @@ import (
 
 func main() {
 	go func() {
-		// Creates a new window
+		// create new window
 		w := app.NewWindow(
-			app.Title("Egg Timer"),
+			app.Title("Egg timer"),
 			app.Size(unit.Dp(400), unit.Dp(600)),
 		)
-		//ops are the operations from the UI
+
+		// ops are the operations from the UI
 		var ops op.Ops
 
-		//startButton is the clickable widget
+		// startButton is a clickable widget
 		var startButton widget.Clickable
 
-		//th defines the material design stype (Theme)
+		// th defines the material design style
 		th := material.NewTheme(gofont.Collection())
 
-		// listin for events in the window we made
+		// listen for events in the window.
 		for e := range w.Events() {
+
+			// detect what type of event
 			switch e := e.(type) {
+
+			// this is sent when the application should re-render.
 			case system.FrameEvent:
 				gtx := layout.NewContext(&ops, e)
-				btn := material.Button(th, &startButton, "Start the timer")
-				btn.Layout(gtx)
+				// Let's try out the flexbox layout:
+				layout.Flex{
+					// Vertical alignment, from top to bottom
+					Axis: layout.Vertical,
+					// Empty space is left at the start, i.e. at the top
+					Spacing: layout.SpaceStart,
+				}.Layout(gtx,
+					// We insert two rigid elements:
+					// First one to hold a button ...
+					layout.Rigid(
+						func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &startButton, "Start")
+							return btn.Layout(gtx)
+						},
+					),
+					// ... then one to hold an empty spacer
+					layout.Rigid(
+						// The height of the spacer is 25 Device independent pixels
+						layout.Spacer{Height: unit.Dp(25)}.Layout,
+					),
+				)
 				e.Frame(gtx.Ops)
 			}
-
 		}
 		os.Exit(0)
-
 	}()
 	app.Main()
 }
