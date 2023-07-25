ee0494377184a964c9fa411a12755404c237f836
Brian <brian@debian>
2023-07-25 13:33:48 -0500

    working egg timer

--- main.go
+++ main.go
@@ -26,16 +26,16 @@ import (
 )
 
 // Define the progress variables, a channel and a variable
-var progressIncrementer chan float32
+var progressIncrementer chan bool
 var progress float32
 
 func main() {
 	// Setup a separate channel to provide ticks to increment progress
-	progressIncrementer = make(chan float32)
+	progressIncrementer = make(chan bool)
 	go func() {
 		for {
 			time.Sleep(time.Second / 25)
-			progressIncrementer <- 0.004
+			progressIncrementer <- true
 		}
 	}()
 
@@ -210,8 +210,12 @@ func draw(w *app.Window) error {
 									var text string
 									if !boiling {
 										text = "Start"
-									} else {
+									}
+									if boiling && progress < 1 {
 										text = "Stop"
+									}
+									if boiling && progress >= 1 {
+										text = "Egg Boiled"
 									}
 									btn := material.Button(th, &startButton, text)
 									return btn.Layout(gtx)
@@ -228,9 +232,12 @@ func draw(w *app.Window) error {
 			}
 
 		// listen for events in the incrementor channel
-		case p := <-progressIncrementer:
+		case <-progressIncrementer:
 			if boiling && progress < 1 {
-				progress += p
+				progress += 1.0 / 25.0 / boilDuration
+				if progress >= 1 {
+					progress = 1
+				}
 				w.Invalidate()
 			}
 		}
